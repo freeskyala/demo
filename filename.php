a9945d0dd0af7e6d94b7b1c0a97af63943f10ff5
tongliang <woshitongliango@126.com>
2014-12-16 08:03:39 +0800

    Git 分享 at 2015

---
+++ filename.php
@@ -0,0 +1,39 @@
+
+centos64 linux下 lnmp的安装思路
+安装时候的目录结构
+/opt/distfiles
+common  database  nginx  php
+
+common中有
+libiconv-1.14.tar.gz   
+libevent-2.0.21-stable.tar.gz    
+libunwind-1.1.tar.gz  
+pcre-8.35.tar.bz2
+
+database中有
+mysql-5.6.20.tar.gz
+
+nginx中有
+nginx-1.7.4
+
+php中有
+ImageMagick.tar.gz            
+mhash-0.9.9.9.tar.bz2  
+php-5.5.15.tar.bz2
+mcrypt-2.6.8.tar.gz  
+libmcrypt-2.5.8.tar.gz       
+php-5.4.33.tar.bz2
+
+1 yum系统常用命令
+http://my.loveying.com/index/index/detail.do?diaryId=7
+2 Samba安装
+http://my.loveying.com/index/index/detail.do?diaryId=11
+3 安装公共库与基础库
+http://my.loveying.com/index/index/detail.do?diaryId=12
+4 nginx安装
+http://my.loveying.com/index/index/detail.do?diaryId=13
+5 mysql安装
+http://my.loveying.com/index/index/detail.do?diaryId=14
+6 依赖包及源码包安装
+http://my.loveying.com/index/index/detail.do?diaryId=15
+http://my.loveying.com/index/index/detail.do?diaryId=116
